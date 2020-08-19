88f68094eee332d4b56d0d594bca0384b66e4409
Bulat <57670154+MadPuffer@users.noreply.github.com>
2020-08-19 23:24:44 +0300

    Add files via upload

--- static/js/configurator.js
+++ static/js/configurator.js
@@ -1,17 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var hardwareBlocks = document.querySelectorAll('.js-hardware-block');
+    let hardwareBlocks = document.querySelectorAll('.js-hardware-block');
+    
+    let motherboardBlock = document.querySelector('.origin-motherboard'),
+        cpuBlock = document.querySelector('.origin-cpu'),
+        ramBlock = document.querySelector('.origin-ram'),
+        gpuBlock = document.querySelector('.origin-gpu'),
+        hddBlock = document.querySelector('.origin-hdd'),
+        psBlock = document.querySelector('.origin-ps'),
+        caseBlock = document.querySelector('.origin-case');
     
     hardwareBlocks.forEach(function(item){
 
       item.addEventListener('click', function(e) {
 
          e.preventDefault();
-         var content = item.querySelector('.container').innerHTML
-         var hardwareBlockClass = item.classList[1];
-         var originBlock = document.querySelector('.origin-' + hardwareBlockClass);
+         let content = item.querySelector('.container').innerHTML
+         let hardwareBlockClass = item.classList[1];
+         let originBlock = document.querySelector('.origin-' + hardwareBlockClass);
          originBlock.innerHTML = content;
+         let closeButton = document.createElement('div');
+         closeButton.innerHTML = '<div class="close-container"><div class="leftright"></div><div class="rightleft"></div></div>';                           
+                                
+         originBlock.querySelector('.hardware-spec').before(closeButton);
           
-         var overlay = document.querySelector('.js-overlay-modal');
+         let overlay = document.querySelector('.js-overlay-modal');
          modalElem = document.querySelector('.modal[data-modal="' + hardwareBlockClass + '"]');
 
          modalElem.classList.remove('active');
